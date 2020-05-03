d3a0a8e8b4fee35dc8774df781d440aab39f8e6c
Vayderk <Vayderk@gmail.com>
2020-05-03 12:42:09 +0200

    First commit

---
+++ MyApp/Gallery/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+	public partial class Form1 : Form
+	{
+
+		int cont = 0;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+		}
+
+		public void chargePhotos()
+		{
+
+			var images = new List<string>() { @"C:\Users\Vayderk\source\repos\MyApp\MyApp\Gallery\Photos\a.jpg",
+				@"C:\Users\Vayderk\source\repos\MyApp\MyApp\Gallery\Photos\b.jpg"};
+
+
+			if (cont > images.Count - 1) { cont = 0; }
+
+			pictureBox1.Image = Image.FromFile(images[cont]);
+			pictureBox1.Width = 400;
+			pictureBox1.Height = 299;
+
+
+
+
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			cont++;
+			chargePhotos();
+		}
+	}
+}
---
+++ MyApp/Index/Front.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace MyApp
+{
+    partial class Front
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Front));
+			this.PanelMenuVertical = new System.Windows.Forms.Panel();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.button10 = new System.Windows.Forms.Button();
+			this.PanelMenuVertical.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// PanelMenuVertical
+			// 
+			this.PanelMenuVertical.BackColor = System.Drawing.Color.RoyalBlue;
+			this.PanelMenuVertical.Controls.Add(this.button10);
+			this.PanelMenuVertical.Controls.Add(this.button9);
+			this.PanelMenuVertical.Controls.Add(this.button8);
+			this.PanelMenuVertical.Controls.Add(this.button7);
+			this.PanelMenuVertical.Controls.Add(this.button6);
+			this.PanelMenuVertical.Controls.Add(this.button5);
+			this.PanelMenuVertical.Controls.Add(this.button4);
+			this.PanelMenuVertical.Controls.Add(this.button3);
+			this.PanelMenuVertical.Controls.Add(this.button2);
+			this.PanelMenuVertical.Controls.Add(this.button1);
+			this.PanelMenuVertical.Controls.Add(this.pictureBox2);
+			this.PanelMenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+			this.PanelMenuVertical.Location = new System.Drawing.Point(0, 0);
+			this.PanelMenuVertical.Name = "PanelMenuVertical";
+			this.PanelMenuVertical.Size = new System.Drawing.Size(160, 450);
+			this.PanelMenuVertical.TabIndex = 0;
+			// 
+			// button9
+			// 
+			this.button9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.button9.Location = new System.Drawing.Point(0, 326);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(160, 35);
+			this.button9.TabIndex = 1;
+			this.button9.Text = "button9";
+			this.button9.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(0, 367);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(160, 35);
+			this.button8.TabIndex = 1;
+			this.button8.Text = "button8";
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(0, 408);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(160, 35);
+			this.button7.TabIndex = 1;
+			this.button7.Text = "button7";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.button6.Location = new System.Drawing.Point(0, 203);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(160, 35);
+			this.button6.TabIndex = 1;
+			this.button6.Text = "Restaurants";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(0, 244);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(160, 35);
+			this.button5.TabIndex = 1;
+			this.button5.Text = "Travels";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(0, 285);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(160, 35);
+			this.button4.TabIndex = 1;
+			this.button4.Text = "Notes";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(0, 162);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(160, 35);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "Contacts";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(0, 121);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(160, 35);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Histories";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.button1.Location = new System.Drawing.Point(0, 80);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(160, 35);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Gallery";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.SystemColors.Info;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(6, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(148, 35);
+			this.pictureBox2.TabIndex = 0;
+			this.pictureBox2.TabStop = false;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(160, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(640, 41);
+			this.panel2.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(56, 52);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// panel3
+			// 
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel3.Location = new System.Drawing.Point(160, 41);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(640, 409);
+			this.panel3.TabIndex = 2;
+			// 
+			// button10
+			// 
+			this.button10.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.button10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.button10.Location = new System.Drawing.Point(0, 41);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(160, 35);
+			this.button10.TabIndex = 1;
+			this.button10.Text = "Nose";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Front
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.PanelMenuVertical);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Front";
+			this.Text = "Form1";
+			this.PanelMenuVertical.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Panel PanelMenuVertical;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button10;
+	}
+}
+
